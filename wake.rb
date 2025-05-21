d0abd01009e3cd7cc062d808bf52c29f88870bb8
samba-rgb <samba24052001@gmail.com>
2025-05-21 22:58:21 +0530

    Update wake formula for Rust project

--- wake.rb
+++ wake.rb
@@ -1,15 +1,17 @@
 class Wake < Formula
-  desc "Wake project"
+  desc "Command-line tool for tailing multiple pods and containers in Kubernetes clusters"
   homepage "https://github.com/samba-rgb/wake"
   url "https://github.com/samba-rgb/wake/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "9a4e3d2d6a3c8fd3af12339842b284f3e0bb7d7421d4e88797f734a4e26e3950"
   license "MIT"
 
+  depends_on "rust" => :build
+
   def install
-    bin.install "wake"
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
-    system "#{bin}/wake", "--version"
+    assert_match "wake", shell_output("#{bin}/wake --version")
   end
 end

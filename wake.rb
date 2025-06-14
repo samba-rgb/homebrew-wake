9b05aac3802347969912ba371e078e9bf815c63f
samba-rgb <samba24052001@gmail.com>
2025-06-15 03:48:01 +0530

    release 2.0

--- wake.rb
+++ wake.rb
@@ -2,7 +2,7 @@ class Wake < Formula
   desc "Command-line tool for tailing multiple pods and containers in Kubernetes clusters"
   homepage "https://github.com/samba-rgb/wake"
   url "https://github.com/samba-rgb/wake/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "b1de16a6e39cd3ba1bde7e501d3c2a8ccce9eecfadc6b96ad94834d8a8d7db68"
+  sha256 "9a6ae4c6c26fc011e113059d092edd3f42e7c317e8627b408d4a9cbe40361829"
   license "MIT"
 
   depends_on "rust" => :build

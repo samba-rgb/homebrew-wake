57b4070e0d9647743fc0e3d83f4ae7048bf3f393
samba-rgb <samba24052001@gmail.com>
2025-07-28 23:52:55 +0530

    Update Wake formula to v0.7.5

--- wake.rb
+++ wake.rb
@@ -1,9 +1,9 @@
 class Wake < Formula
   desc "Command-line tool for tailing multiple pods and containers in Kubernetes clusters"
   homepage "https://github.com/samba-rgb/wake"
-  url "https://github.com/samba-rgb/wake/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "93bb510a8ea4d65edef7df3003f7ef6edbca665f0eef430068cd6a3b9b64da06"
-  version "0.7.2"
+  url "https://github.com/samba-rgb/wake/archive/refs/tags/v0.7.5.tar.gz"
+  sha256 "bfe7e603bfd632a8eca643253b2d88c0b3729822cdedc592b8a73bf4ec330cdd"
+  version "0.7.5"
   license "MIT"
 
   depends_on "rust" => :build
@@ -20,7 +20,7 @@ class Wake < Formula
              "https://github.com/samba-rgb/wake/archive/refs/tags/#{requested_version}.tar.gz"
       system "tar", "-xzf", "wake-#{requested_version}.tar.gz", "--strip-components=1"
     else
-      ohai "Installing Wake version 0.7.2 (latest)"
+      ohai "Installing Wake version 0.7.5 (latest)"
     end
 
     system "cargo", "install", "--root", prefix, "--path", "."

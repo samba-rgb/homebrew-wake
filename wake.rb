ebc5aa37d214a648d8cb61e9f9a6b954800c8d07
samba-rgb <samba24052001@gmail.com>
2025-11-23 21:24:58 +0530

    Update Wake formula to use pre-built binaries for ARM systems with Intel chip error messages

--- wake.rb
+++ wake.rb
@@ -6,7 +6,7 @@ class Wake < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/samba-rgb/wake/releases/download/v0.9.11/wake-aarch64-apple-darwin.tar.gz"
-      sha256 "UPDATE_WITH_ARM_MAC_SHA"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       # Intel Mac not supported yet
       odie "Wake is not available for Intel Mac chips yet. Please use an ARM-based Mac (Apple Silicon)."
@@ -16,7 +16,7 @@ class Wake < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/samba-rgb/wake/releases/download/v0.9.11/wake-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "UPDATE_WITH_ARM_LINUX_SHA"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       # Intel Linux not supported yet
       odie "Wake is not available for Intel Linux chips yet. Please use an ARM-based Linux system."
